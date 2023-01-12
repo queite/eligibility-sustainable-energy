030143764d2e32d1399dd7fdbdad5717a65c5854
Queite <queitesc@gmail.com>
2023-01-12 11:28:08 -0300

    test: :test_tube: adds first test cases

---
+++ tests/unit/checkMinConsumption.test.ts
@@ -0,0 +1,8 @@
+describe('CheckTaxClass', () => {
+  it('should return true if elegible', () => {
+
+  })
+  it('should return "Consumo muito baixo para tipo de conexão" if inelegible', () => {
+
+  })
+})
---
+++ tests/unit/checkTaxClass.test.ts
@@ -0,0 +1,8 @@
+describe('CheckTaxClass', () => {
+  it('should return true if elegible', () => {
+
+  })
+  it('should return "Modalidade tarifária não aceita" if inelegible', () => {
+
+  })
+})
