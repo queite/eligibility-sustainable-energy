a6a1e1aabff3a0c563868598998a824362e5bc48
Queite <queitesc@gmail.com>
2023-01-12 11:52:04 -0300

    test: :white_check_mark: implements unit test to CheckTaxClass

--- tests/unit/checkTaxClass.test.ts
+++ tests/unit/checkTaxClass.test.ts
@@ -1,8 +1,13 @@
+import { expect } from "chai";
+import CheckTaxClass from "../../src/domain/checkers/CheckTaxClass";
+
 describe('CheckTaxClass', () => {
   it('should return true if elegible', () => {
-
+    const checkTaxClient = new CheckTaxClass();
+    expect(checkTaxClient.checkEligibility('branca')).to.be.equal(true)
   })
   it('should return "Modalidade tarifária não aceita" if inelegible', () => {
-
+     const checkTaxClient = new CheckTaxClass();
+     expect(checkTaxClient.checkEligibility('rural')).to.be.equal('Modalidade tarifária não aceita')
   })
 })
