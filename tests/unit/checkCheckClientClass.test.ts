d9d9913cb623557b621f80c0f0927224dad059d4
Queite <queitesc@gmail.com>
2023-01-15 16:20:25 -0300

    refactor: :recycle: updates tests from branch api

--- tests/unit/checkCheckClientClass.test.ts
+++ tests/unit/checkCheckClientClass.test.ts
@@ -1,13 +1,14 @@
-import { expect } from "chai"
-import CheckClientClass from "../../src/domain/checkers/CheckClientClass"
+import { expect } from "chai";
+import CheckClientClass from "../../src/domain/checkers/CheckClientClass";
 
 describe('CheckClientClass', () => {
   it('should return true if elegible', () => {
-    const checkClientClass = new CheckClientClass()
-    expect(checkClientClass.checkEligibility('residencial')).to.be.equal(true)
-  })
+    const checkClientClass = new CheckClientClass();
+    expect(checkClientClass.checkEligibility('residencial')).to.be.equal(true);
+  });
+
   it('should return "Classe de consumo não aceita" if inelegible', () => {
-    const checkClientClass = new CheckClientClass()
-    expect(checkClientClass.checkEligibility('rural')).to.be.equal('Classe de consumo não aceita')
-  })
-})
+    const checkClientClass = new CheckClientClass();
+    expect(checkClientClass.checkEligibility('rural')).to.be.equal('Classe de consumo não aceita');
+  });
+});
