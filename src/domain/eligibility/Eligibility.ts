f4e368cac92b1c34ff0257d5fd1f7fcee3d07c73
Queite <queitesc@gmail.com>
2023-01-15 17:25:01 -0300

    refactor: :recycle: updates from main

--- src/domain/eligibility/Eligibility.ts
+++ src/domain/eligibility/Eligibility.ts
@@ -30,30 +30,35 @@ export default class Eligibility {
     this.connectionType = clientData.tipoDeConexao;
     this.checkClientClass = new CheckClientClass();
     this.checkMinConsumption = new CheckMinConsumption(this.consumptionHistory);
-    this.checkTaxClass = new CheckTaxClass()
+    this.checkTaxClass = new CheckTaxClass();
     this.ineligibility = [];
-  }
+  };
 
   private CO2Calc() {
     const consumptionSum = this.consumptionHistory.reduce((acc, current) => acc + current);
-    return Number(((consumptionSum * 84) / 1000).toFixed(2))
-  }
+    if (this.consumptionHistory.length < 12) {
+      const consumptionAverage = consumptionSum / this.consumptionHistory.length;
+      const annualConsumptionProjection = consumptionAverage * 12;
+      return Number(((annualConsumptionProjection * 84) / 1000).toFixed(2));
+    };
+    return Number(((consumptionSum * 84) / 1000).toFixed(2));
+  };
 
   checkEligibility() {
     const checkResults = [
       this.checkClientClass.checkEligibility(this.clientClass),
       this.checkMinConsumption.checkEligibility(this.connectionType),
       this.checkTaxClass.checkEligibility(this.taxClass)
-    ]
-    this.ineligibility = checkResults.filter((result) => result !== true) as Ineligibility
+    ];
+    this.ineligibility = checkResults.filter((result) => result !== true) as Ineligibility;
     if (this.ineligibility.length > 0) {
       return {
       elegivel: false,
       razoesInelegibilidade: this.ineligibility};
-    }
+    };
       return {
       elegivel: true,
       economiaAnualDeCO2: this.CO2Calc()
-    }
-  }
-}
+    };
+  };
+};
--- tests/mocks/eligibilitiMocks.ts
+++ tests/mocks/eligibilitiMocks.ts
@@ -1,11 +1,11 @@
-import { Input } from "../../src/types"
+import { Input } from '../../src/types';
 
 export const eligibleClient: Input = {
-  "numeroDoDocumento": "14041737706",
-  "tipoDeConexao": "bifasico",
-  "classeDeConsumo": "comercial",
-  "modalidadeTarifaria": "convencional",
-  "historicoDeConsumo": [
+  'numeroDoDocumento': '14041737706',
+  'tipoDeConexao': 'bifasico',
+  'classeDeConsumo': 'comercial',
+  'modalidadeTarifaria': 'convencional',
+  'historicoDeConsumo': [
     3878, // mes atual
     9760, // mes anterior
     5976, // 2 meses atras
@@ -19,14 +19,14 @@ export const eligibleClient: Input = {
     6941, // 10 meses atras
     4597  // 11 meses atras
   ]
-}
+};
 
 export const ineligibleClient: Input = {
-  "numeroDoDocumento": "14041737706",
-  "tipoDeConexao": "bifasico",
-  "classeDeConsumo": "rural",
-  "modalidadeTarifaria": "verde",
-  "historicoDeConsumo": [
+  'numeroDoDocumento': '14041737706',
+  'tipoDeConexao': 'bifasico',
+  'classeDeConsumo': 'rural',
+  'modalidadeTarifaria': 'verde',
+  'historicoDeConsumo': [
     3878, // mes atual
     9760, // mes anterior
     5976, // 2 meses atras
@@ -38,4 +38,22 @@ export const ineligibleClient: Input = {
     7859, // 8 meses atras
     4160, // 9 meses atras
   ]
-}
+};
+
+export const eligibleClientWithLessConsumptionRegister: Input = {
+  'numeroDoDocumento': '14041737706',
+  'tipoDeConexao': 'bifasico',
+  'classeDeConsumo': 'comercial',
+  'modalidadeTarifaria': 'convencional',
+  'historicoDeConsumo': [
+    3878, // mes atual
+    9760, // mes anterior
+    5976, // 2 meses atras
+    2797, // 3 meses atras
+    2481, // 4 meses atras
+    5731, // 5 meses atras
+    7538, // 6 meses atras
+    4392, // 7 meses atras
+    7859, // 8 meses atras
+  ]
+};
