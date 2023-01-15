2e8842cbab21331185ab3354a5c57c3778bbf29a
Queite <queitesc@gmail.com>
2023-01-15 17:06:14 -0300

    fix: :bug: implements logic to CO2 annual economy when consumption is less than twelve

--- tests/unit/eligibility.test.ts
+++ tests/unit/eligibility.test.ts
@@ -1,14 +1,26 @@
-import { expect } from "chai"
-import Eligibility from "../../src/domain/eligibility/Eligibility"
-import { eligibleClient, ineligibleClient } from "../mocks/eligibilitiMocks"
+import { expect } from 'chai';
+import Eligibility from '../../src/domain/eligibility/Eligibility';
+import {
+  eligibleClient,
+  eligibleClientWithLessConsumptionRegister,
+  ineligibleClient
+} from '../mocks/eligibilitiMocks';
 
 describe('Eligibility', () => {
   it('should return object with key "economiaAnualDeCO2" if client is eligible', () => {
-    const checkEligibility = new Eligibility(eligibleClient)
-    expect(checkEligibility.checkEligibility()).to.have.property('economiaAnualDeCO2')
-  })
-  it('should return object with key razoesInelegibilidade if client is ineligible', () => {
-    const checkEligibility = new Eligibility(ineligibleClient)
-    expect(checkEligibility.checkEligibility()).to.have.property('razoesInelegibilidade')
+    const checkEligibility = new Eligibility(eligibleClient);
+    expect(checkEligibility.checkEligibility()).to.have.property('economiaAnualDeCO2');
+  });
+
+  it('should return object with key "razoesInelegibilidade" if client is ineligible', () => {
+    const checkEligibility = new Eligibility(ineligibleClient);
+    expect(checkEligibility.checkEligibility()).to.have.property('razoesInelegibilidade');
+  });
+
+  it('should return annual projection based on average if consumption history is less than twelve',
+    () => {
+      const checkEligibility = new Eligibility(eligibleClientWithLessConsumptionRegister);
+      expect(checkEligibility.checkEligibility())
+        .to.have.deep.eq({'elegivel': true, 'economiaAnualDeCO2': 5646.14});
   })
 })
